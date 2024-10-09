bab80947792c60c9d3a31fa102baea4744a5fd84
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2024-10-09 13:25:58 +0300

    dd

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+// jj
 class Region extends Model
 {
     use HasFactory,SoftDeletes;
