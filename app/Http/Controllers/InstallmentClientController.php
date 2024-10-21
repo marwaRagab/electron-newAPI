18d7fff1ae469292055d4cd6c9fb25079afd0319
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2024-10-21 16:55:24 +0300

    installment

--- app/Http/Controllers/InstallmentClientController.php
+++ app/Http/Controllers/InstallmentClientController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use Illuminate\Http\Request;
 use App\Models\Installment_Client;
+use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\StoreInstallment_ClientRequest;
 use App\Http\Requests\UpdateInstallment_ClientRequest;
 use App\Interfaces\InstallmentClientsRepositoryInterface;
@@ -23,74 +28,126 @@ class InstallmentClientController extends Controller
      }
 
      
-    public function index()
-    {
-        //
-    }
+     public function index()
+     {
+         //
+         $data = $this->InstallmentClientsRepository->index();
+         // return response()->json($permissions);
+         return $this->respondSuccess($data, 'Get Data successfully.');
+     }
+  
+     public function getall(Request $request)
+     {
+         if ($request->ajax()) {
+             $data = Installment_Client::select('*');
+             return DataTables::of($data)->toJson();
+         }
+     }
+  
+     /**
+      * Show the form for creating a new resource.
+      *
+      * @return \Illuminate\Http\Response
+      */
+  
+      public function create()
+     {
+         //
+     }
+  
+     public function store(Request $request)
+     {
+         $messages = [
+             'name_ar.required' => 'الاسم بالعربى  مطلوب.',
+             'name_en.required' => 'الاسم بالانجليزية  مطلوب.',
+             'governorate_id.required' => 'المحافظة   مطلوب.',
+         ];
+  
+         $validatedData = Validator::make($request->all(), [
+             'name_ar' => 'required',
+             'name_en' => 'required',
+             'governorate_id' =>'required'
+         ], $messages);
+  
+         if ($validatedData->fails()) {
+  
+             return $this->respondError('Validation Error.', $validatedData->errors(), 400);
+         }
+         $data = $this->InstallmentClientsRepository->store($request);
+         // return response()->json($nationalities);
+         return $this->respondSuccess(result: $data, message: 'Store Data successfully.');
+     }
+  
+     
+    
+  
+     /**
+      * Display the specified resource.
+      *
+      * @param  \App\Models\InstallmentClients  $InstallmentClients
+      * @return \Illuminate\Http\Response
+      */
+     public function show($id)
+     {
+         $data = $this->InstallmentClientsRepository->show($id);
+         // return response()->json($data);
+         return $this->respondSuccess($data, 'Get Data successfully.');
+  
+     }
+  
+     /**
+      * Show the form for editing the specified resource.
+      *
+      * @param  \App\Models\InstallmentClients  $InstallmentClients
+      * @return \Illuminate\Http\Response
+      */
+     public function edit($id)
+     {
+         $data = $this->InstallmentClientsRepository->show($id);
+         // return response()->json($data);
+         return $this->respondSuccess($data, message: 'Get Data successfully.');
+  
+     }
+  
+     /**
+      * Update the specified resource in storage.
+      *
+      * @param  \App\Http\Requests\UpdateInstallmentClientsRequest  $request
+      * @param  \App\Models\InstallmentClients  $InstallmentClients
+      * @return \Illuminate\Http\Response
+      */
+     public function update($id ,  Request $request)
+     {
+         //
+         $data = $this->InstallmentClientsRepository->update($id  ,$request);
+         // return response()->json($data);
+         return $this->respondSuccess($data, 'Update Data successfully.');
+  
+     }
+  
+     /**
+      * Remove the specified resource from storage.
+      *
+      * @param  \App\Models\InstallmentClients  $InstallmentClients
+      * @return \Illuminate\Http\Response
+      */
+     public function destroy($id)
+     {
+         //
+         $data = $this->InstallmentClientsRepository->destroy($id);
+         // return response()->json($data);
+         return $this->respondSuccess($data, message: 'Delete Data successfully.');
+     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function check_client($civil_number)
     {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreInstallment_ClientRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreInstallment_ClientRequest $request)
-    {
-        //
-    }
+        $data = Client::where('civil_number',$civil_number)->first();
+        if($data)
+        {
+            return $this->respondSuccess(result: $data, message: 'fetch Data successfully.');
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Installment_Client  $installment_Client
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Installment_Client $installment_Client)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Installment_Client  $installment_Client
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Installment_Client $installment_Client)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateInstallment_ClientRequest  $request
-     * @param  \App\Models\Installment_Client  $installment_Client
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateInstallment_ClientRequest $request, Installment_Client $installment_Client)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Installment_Client  $installment_Client
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Installment_Client $installment_Client)
-    {
-        //
+        return $this->respondError('Error.', 'failed to fetch Data', 400);
     }
 }
